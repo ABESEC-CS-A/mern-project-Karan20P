a79c67b5a05e7119e05dfea404f736b36a9c1320
Karan20P <karan24800@gmail.com>
2025-11-12 15:37:14 +0530

    made components

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="about">About</Link>
+      <Link to="contact">Contact</Link>
+      <Link to="login">Login</Link>
+      <Link to="register">Register</Link>
+    </div>
+  );
+};
+
+export default Navbar;
